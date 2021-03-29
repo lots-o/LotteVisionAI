46762a59baca48722ec251197b993346101f7dd5
lots-o <kgh0916@gmail.com>
2021-03-29 11:57:05 +0900

    Modified typo

--- config.py
+++ config.py
@@ -5,10 +5,6 @@ weight_decay=1e-2
 n_epochs=20
 k_fold=3
 model_name='efficientnet-b3'
-<<<<<<< HEAD
-=======
-
->>>>>>> 0d925aa9e30e30d8ca11cd6f1c72c88c34ceed87
 
 log_dir='Log/'
 root_dir='train/'
